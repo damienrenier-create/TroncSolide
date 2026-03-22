0cd6938edcccd3832186e5cb948a5fe5f169af09
damienrenier-create <damienrenier@hotmail.com>
2026-03-22 14:18:43 +0100

    debug: add test route and error handling on home page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Routing works! 🌳</h1>
+      <p>If you can see this, Next.js routing is functional on Vercel.</p>
+    </div>
+  );
+}
